0777c2419f8402f6eebc68713ff53227d97df5a3
R <ryansereno@protonmail.com>
2022-08-25 13:38:25 -0400

    added card wraapper

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,8 +1,9 @@
 import ExpenseItem from './ExpenseItem'
+import Card from './Card'
 import './Expenses.css'
 
 function Expenses(props){
-    return <div className="expenses">
+    return <Card className="expenses">
 
       <ExpenseItem
         title={props.items[0].title}
@@ -25,7 +26,7 @@ function Expenses(props){
         date={props.items[3].date}
       ></ExpenseItem>
 
-        </div>
+        </Card>
 }
 
 export default Expenses;
