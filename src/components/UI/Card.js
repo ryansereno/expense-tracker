dba29ffd999e9daf8ca105264ae1dcd0f70ba249
R <ryansereno@protonmail.com>
2022-08-26 12:44:43 -0400

    reorganized directories, and added state to ExpenseItem component

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,6 +1,6 @@
 import './Card.css' 
 
-function Card(props){
+const Card = (props) => {
     const classes = 'card ' + props.className //must allow classes to be applied at each instance of Card, in the same way classes can be applied to standard html tags like div
     return <div className={classes}>{props.children}</div>
 
