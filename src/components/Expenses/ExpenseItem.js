7302a4467b852e2212e60e6149e7693701c7ca9a
R <ryansereno@protonmail.com>
2022-08-27 11:40:04 -0400

    removed state from expenseItem

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,17 @@
 
-import React, { useState  } from 'react'
+import React from 'react'
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-
-    const [title, setTitle] = useState(props.title)
-    //state is evaluated on a PER COMPONENT basis; separate instances of the same component have SEPARATE state
-    const clickHandler = () =>{
-       setTitle('clicked') 
-    }
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
